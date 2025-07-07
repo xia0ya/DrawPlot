987840e77f05b16cd2bf8942f599b2165b124bae
xiaoya <2949774174@qq.com>
2025-07-07 19:34:52 +0800

    解决图片不显示

--- src/data/charts.js
+++ src/data/charts.js
@@ -3,55 +3,55 @@ export const charts = [
   {
     id: 'map-of-r-two',
     title: 'Map-of-R-two',
-    image: '/images/Map-of-R-two.png',
+    image: 'images/Map-of-R-two.png',
     markdown: 'Map-of-R-two.md'
   },
   {
     id: 'fa-netpic',
     title: 'Fa_NetPic',
-    image: '/images/Fa_NetPic.png',
+    image: 'images/Fa_NetPic.png',
     markdown: 'Fa_NetPic.md'
   },
   {
     id: 'bubble-chart',
     title: '层叠气泡图',
-    image: '/images/层叠气泡图.png',
+    image: 'images/层叠气泡图.png',
     markdown: 'bubble-chart.md'
   },
   {
     id: 'pca',
     title: 'PCA',
-    image: '/images/PCA.png',
+    image: 'images/PCA.png',
     markdown: 'PCA.md'
   },
   {
     id: 'map-of-r',
     title: 'Map-of-R',
-    image: '/images/Map-of-R.png',
+    image: 'images/Map-of-R.png',
     markdown: 'Map-of-R.md'
   },
   {
     id: 'dot-bubble',
     title: '点型网状气泡图',
-    image: '/images/点型网状气泡图.png',
+    image: 'images/点型网状气泡图.png',
     markdown: 'dot-bubble.md'
   },
   {
     id: 'bubble-chart2',
     title: '层叠气泡图2',
-    image: '/images/层叠气泡图2.png',
+    image: 'images/层叠气泡图2.png',
     markdown: 'bubble-chart2.md'
   },
   {
     id: 'trash-wheel-2',
     title: '2024-03-05_Trash_Wheel_Collection_DataRplot2',
-    image: '/images/2024-03-05_Trash_Wheel_Collection_DataRplot2.jpeg',
+    image: 'images/2024-03-05_Trash_Wheel_Collection_DataRplot2.jpeg',
     markdown: 'Trash_Wheel_Collection_DataRplot2.md'
   },
   {
     id: 'trash-wheel-1',
     title: '2024-03-05_Trash_Wheel_Collection_DataRplot1',
-    image: '/images/2024-03-05_Trash_Wheel_Collection_DataRplot1.jpeg',
+    image: 'images/2024-03-05_Trash_Wheel_Collection_DataRplot1.jpeg',
     markdown: 'Trash_Wheel_Collection_DataRplot1.md'
   }
 ]; 
